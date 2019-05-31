ae2564543579394adac48202679da76b9a9a0fcb
yiranyyu <2606375857@qq.com>
2019-05-31 10:33:00 +0800

    init project

---
+++ src/User.java
@@ -0,0 +1,30 @@
+/**
+ * @author Yirany
+ * @version 1.0
+ * @since 5/31/2019
+ **/
+public class User {
+    private String name;
+    private String ip;
+
+    public User(String name, String ip) {
+        this.name = name;
+        this.ip = ip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
