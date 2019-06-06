c18e6072e534be00f7b51895972d6158bf6555b6
yiranyyu <2606375857@qq.com>
2019-06-06 20:12:31 +0800

    Add doc by 岑少锋

--- src/MessageDB.java
+++ src/MessageDB.java
@@ -1,63 +1,96 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+/**
+* 这个类与数据库中的Msg表的表项对应
+* @author 岑少锋
+*/
 
 public class MessageDB {
-    private int id;
-    private String senderID;
-    private String receiverID;
-    private String time;
-    private String text;
-    /* two options
-    /  1. Text
-    /  2. Pic
-    */
-    private String type;
+	private int id;
+	private int senderid;
+	private int receiverid;
+	private String time;
+	private String text;
 
-    public int getID() {
-        return this.id;
-    }
+	public MessageDB(int id,int senderid,int receiverid,String time,String text) {
+		this.id = id;
+		this.senderid = senderid;
+		this.receiverid = receiverid;
+		this.time = time;
+		this.text = text;
+	}
+   /**
+   * This method set the value of ID
+   * @param id The value of message id.
+   * @return Nothing.
+   */
+	public void setID(int id) {
+		this.id = id;
+	}
+   /**
+   * This method Return the value of ID
+   * @return The value of Id.
+   */
+	public int getID() {
+		return this.id;
+	}
+   /**
+   * This method set the value of SenderID
+   * @param senderid The value of Sender id.
+   * @return Nothing.
+   */
+	public void setSenderID(int senderid) {
+		this.senderid = senderid;
+	}
+   /**
+   * This method Return the value of SenderID
+   * @return The value of Sender Id.
+   */
+	public int getSenderID() {
+		return this.senderid;
+	}
+   /**
+   * This method set the value of ReceiverID
+   * @param receiverid The value of Receiver id.
+   * @return Nothing.
+   */
+	public void setReceiverID(int receiverid) {
+		this.receiverid = receiverid;
+	}
+   /**
+   * This method Return the value of ReceiverID
+   * @return The value of Receiver Id.
+   */
+	public int getReceiverID() {
+		return this.receiverid;
+	}
+   /**
+   * This method set the value of Time
+   * @param time The value of Time.
+   * @return Nothing.
+   */
+	public void setTime(String time) {
+		this.time = time;
+	}
+   /**
+   * This method Return the value of Time as String
+   * @return The value of time.
+   */
+	public String getTime() {
+		return this.time;
+	}
+   /**
+   * This method set the value of text
+   * @param text The value of Receiver id.
+   * @return Nothing.
+   */
+	public void setText(String text) {
+		this.text = text;
+	}
+   /**
+   * This method Return the value of text
+   * @return The value of test.
+   */
+	public String getText() {
+		return this.text;
+	}
 
-    void setID(int id) {
-        this.id = id;
-    }
-
-    public String getSenderID() {
-        return this.senderID;
-    }
-
-    void setSenderID(String senderID) {
-        this.senderID = senderID;
-    }
-
-    public String getReceiverID() {
-        return this.receiverID;
-    }
-
-    void setReceiverID(String receiverID) {
-        this.receiverID = receiverID;
-    }
-
-    public String getTime() {
-        return this.time;
-    }
-
-    void setTime(long time) {
-        this.time = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(new Date(time));
-    }
-
-    public String getText() {
-        return this.text;
-    }
-
-    void setText(String text) {
-        this.text = text;
-    }
-
-    public String getType() {
-        return this.type;
-    }
-
-    void setType(String type) {
-        this.type = type;
-    }
 }
--- src/ProcessSQL.java
+++ src/ProcessSQL.java
@@ -1,150 +1,227 @@
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
-
+import java.util.Date;
 import java.sql.*;
 
-class ProcessSQL {
-    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://39.105.80.32/ChatDB";
+/**
+* 这个类用于远程连接并操作MySQL数据库
+* @author 岑少锋
+*/
 
-    private static final String USER = "root";
-    private static final String PASS = "123456Abc!hhh";
-
-    private Connection conn = null;
-    private Statement stmt = null;
-
-    ProcessSQL() {
-        try {
-            Class.forName(JDBC_DRIVER);
-
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            System.out.println("Connected!");
-
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-            System.out.println("Connected!");
-
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    boolean ContainsName(String username) {
-        boolean flag = true;
-        try {
+public class ProcessSQL {
+	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String DB_URL = "jdbc:mysql://39.105.80.32/ChatDB";
+	private static final String USER = "root";
+	private static final String PASS = "123456Abc!hhh";
+	private Connection conn = null;
+	private Statement stmt = null;
+   /**
+   * This constructor connects mysql database
+   */
+	public ProcessSQL() {
+		try {
+			Class.forName(JDBC_DRIVER);
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+		} catch(SQLException se) {
+			se.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+   /**
+   * This method judges if the username exists
+   * @param username username needs to be judged.
+   * @return boolean.
+   */
+	public boolean ContainsName(String username) {
+		boolean flag = true;
+		try {
             String sql = String.format("SELECT * FROM User WHERE username = '%s'", username);
-            ResultSet rs = stmt.executeQuery(sql);
-            flag = rs.next();
-            rs.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return flag;
-    }
-
-    private void ExecuteSqlAndBuildJsonArray(String sql, JSONArray jsonArray) {
-        try {
-            ResultSet rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String senderid = rs.getString("senderid");
-                String receiverid = rs.getString("receiverid");
-                long time = rs.getLong("time");
-                String text = rs.getString("text");
-                String type = rs.getString("type");
-
-                MessageDB msg = new MessageDB();
-                msg.setID(id);
-                msg.setSenderID(senderid);
-                msg.setReceiverID(receiverid);
-                msg.setTime(time);
-                msg.setText(text);
-                msg.setType(type);
-
-                JSONObject jsonObject = JSONObject.fromObject(msg);
-                jsonArray.add(jsonObject);
-            }
-            rs.close();
-        } catch (SQLException e) {
-        }
-    }
-
-    JSONArray PullMsg(String sender, String receiver) {
-        JSONArray jsonArray = new JSONArray();
-        String sql = "";
-        if (receiver == null)
-            sql = "SELECT * FROM MSG WHERE senderid = " + sender + " OR receiverid = " + sender;
-        else
-            sql = "SELECT * FROM MSG WHERE (senderid = " + sender + " AND receiver = " + receiver + ") OR (senderid = " + receiver + " AND receiver = " + sender + ")";
-        this.ExecuteSqlAndBuildJsonArray(sql, jsonArray);
-        return jsonArray;
-    }
-
-    JSONArray Login(String username, String password)
-            throws UserNotExistsException, PasswordErrorException {
-        try {
-            String sql = String.format( "SELECT password from USER WHERE username = '%s'",username);
-            ResultSet rs = stmt.executeQuery(sql);
-            if (rs.next()) {
-                String pw = rs.getString("password");
-                if (password.equals(pw)) {
-                    return this.PullMsg(username, null);
-                } else
-                    throw new PasswordErrorException(username, password);
-            } else
-                throw new UserNotExistsException(username);
-        } catch (SQLException e) {
-        }
-        return null;
-    }
-
-    void SignUp(String username, String password)
-            throws UserAlreadyExistsException, DatabaseInsertFailException {
-        if (ContainsName(username))
-            throw new UserAlreadyExistsException(username);
-        else {
-            if (InsertUser(username, password))
-                throw new DatabaseInsertFailException();
-        }
-    }
-
-    private boolean InsertUser(String username, String password) {
-        try {
-            String sql = "INSERT INTO USER VALUES(\"" + username + "\", \"" + password + "\")";
-            int rs = stmt.executeUpdate(sql);
-            if (rs > 0)
-                return true;
-            else
-                return false;
-        } catch (SQLException e) {
-
-        }
-        return true;
-    }
-
-    void close() {
-        try {
-            stmt.close();
-            conn.close();
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (stmt != null)
-                    stmt.close();
-            } catch (SQLException se2) {
-            }
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
-    }
+			ResultSet rs = stmt.executeQuery(sql);
+			flag = rs.next();
+			rs.close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+   /**
+   * This method executes sql statement and return data organized by JSONArray
+   * @param sql sql statement.
+   * @return JSONArray.
+   */
+	private JSONArray ExecuteSqlAndBuildJsonArray(String sql) {
+		JSONArray jsonArray = new JSONArray();
+		try {
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				MessageDB msg = new MessageDB(rs.getInt("id"),
+									  		  rs.getInt("sender"),
+									  		  rs.getInt("receiver"),
+									  		  rs.getString("time"),
+									  		  rs.getString("content"));
+				
+				JSONObject jsonObject = JSONObject.fromObject(msg);
+				jsonArray.add(jsonObject);
+			}
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return jsonArray;
+	}
+   /**
+   * This method pulls message from mysql databases
+   * @param sender a user in the chat.
+   * @param receiver a user in the chat.
+   * @return Nothing.
+   */	
+	public JSONArray PullMsg(String sender, String receiver) {
+		int senderid = this.findIdByusername(sender);
+		int receiverid = this.findIdByusername(receiver);
+		String sql = "";
+		if (receiver == null) 
+			sql = String.format("SELECT * FROM Msg WHERE sender = %d OR receiver = %d", senderid, senderid);
+		else 
+			sql = String.format("SELECT * FROM Msg WHERE (sender = %d AND receiver = %d) OR (sender = %d AND receiver = %d)", senderid , receiverid, receiverid, senderid);
+		return this.ExecuteSqlAndBuildJsonArray(sql);
+	}
+   /**
+   * This method verifies account and return data organized by JSONArray
+   * @param username username of the account.
+   * @param password password of the account. 
+   * @return JSONArray.
+   * @exception UserNotExistsException On username not exist error.
+   * @exception PasswordErrorException On password wrong error.
+   */	
+	public JSONArray Login(String username, String password) 
+			throws UserNotExistsException, PasswordErrorException {
+		try {
+            String sql = String.format("SELECT password from User WHERE username='%s'", username);
+			ResultSet rs = stmt.executeQuery(sql);
+			if(rs.next()) {
+				String pw = rs.getString("password");
+				if(password.equals(pw) == true) {
+					return this.PullMsg(username, null);
+				} else 
+					throw new PasswordErrorException(username, password);
+			} else 
+				throw new UserNotExistsException(username);
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+   /**
+   * This method checks the username and add user data to the database
+   * @param username username of the account.
+   * @param password password of the account.
+   * @return Nothing.
+   * @exception UserAlreadyExistsException On duplicate username error.
+   * @exception DatabaseInsertFailException On insert data to database error.
+   */
+	public void SignUp(String username, String password) 
+			throws UserAlreadyExistsException, DatabaseInsertFailException {
+		if(this.ContainsName(username) == true) 
+			throw new UserAlreadyExistsException(username);
+		else {
+			if (this.InsertUser(username, password) == false) 
+				throw new DatabaseInsertFailException();
+		}
+	}
+   /**
+   * This method adds message data to the database
+   * @param sendername sender's username.
+   * @param receivername receiver's username.
+   * @param content content in the message.
+   * @return boolean.
+   * @exception UserNotExistsException On username not exist error.
+   */
+	public boolean SendMsg(String sendername, String receivername, String content) 
+			throws UserNotExistsException {
+		int senderid = this.findIdByusername(sendername);
+		int receiverid = this.findIdByusername(receivername);
+		if(senderid == -1)
+			throw new UserNotExistsException(sendername);
+		if(receiverid == -1)
+			throw new UserNotExistsException(receivername);
+		
+		try {
+			String sql = String.format("insert into Msg(sender,receiver,content,time) Values(%d,%d,'%s','%s')",senderid,receiverid,content,new Date());
+			int rs = stmt.executeUpdate(sql);
+			if(rs > 0)
+				return true;
+			else
+				return false;
+		} catch(SQLException e) {
+		}
+		return true;
+	}
+   /**
+   * This method finds username by userId in database
+   * @param username username of user.
+   * @return int userid of a user -1 means not exist.
+   */	
+	private int findIdByusername(String username) {
+		if (username == null)
+			return -1;
+		try {
+			String sql = String.format("SELECT id from User WHERE username = '%s'", username);
+			ResultSet rs = stmt.executeQuery(sql);
+			if(rs.next())
+				return rs.getInt("id");
+			else 
+				return -1;
+		} catch (SQLException e) {
+		}
+		return -1;
+	}
+   /**
+   * This method inserts a user data to the database
+   * @param username username of a user.
+   * @param password password of a user.
+   * @return boolean.
+   */
+	private boolean InsertUser(String username, String password) {
+		try {
+			String sql = String.format("insert into User(username,password) Values('%s','%s')", username, password);
+			int rs = stmt.executeUpdate(sql);
+			if(rs > 0)
+				return true;
+			else
+				return false;
+		} catch(SQLException e) {
+		}
+		return true;
+	}
+   /**
+   * This method closes the connection to the databasae
+   * @return Nothing.
+   */
+	void close() {
+		try {
+			stmt.close();
+			conn.close();
+		} catch(SQLException se) {
+			se.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try{
+				if(stmt!=null)
+					stmt.close();
+			}catch(SQLException se2){
+			}
+			try{
+				if(conn!=null)
+					conn.close();
+			}catch(SQLException se){
+				se.printStackTrace();
+			}
+		}
+	}
 }
